c3dbc11ebfd5c01001290dc75676d9779d72b82f
whyfai <74695568+whyfai@users.noreply.github.com>
2021-11-16 20:07:28 +0700

    Add invite command

--- bot.py
+++ bot.py
@@ -9,6 +9,10 @@ bot = commands.Bot(command_prefix=when_mentioned_or("w!", "w?"), activity=discor
 async def ping(ctx):
     await ctx.reply(f"🏓Pong! `{round(bot.latency*1000)}ms`")
 
+@bot.command()
+async def invite(ctx):
+    await ctx.reply("With slash commands: https://discord.com/api/oauth2/authorize?client_id=775601025272905739&permissions=8&scope=bot%20applications.commands \nWithout slash commands: https://discord.com/api/oauth2/authorize?client_id=775601025272905739&permissions=8&scope=bot\n\nIf you are trying to add slash commands to a server that already has the bot use this link: https://discord.com/api/oauth2/authorize?client_id=775601025272905739&scope=applications.commands")
+
 bot.load_extension("jishaku")
 
 bot.run(config.token)
