0229231c85a37bc0129e048e4e6c3ffd2147b0ee
whyfai <74695568+whyfai@users.noreply.github.com>
2021-11-16 19:40:44 +0700

    Initial Bot Setup

---
+++ bot.py
@@ -0,0 +1,12 @@
+import config
+import discord
+from discord.ext import commands
+from discord.ext.commands.bot import when_mentioned_or
+
+bot = commands.Bot(command_prefix=when_mentioned_or("w!", "w?"), activity=discord.Activity(type=discord.ActivityType.playing, name="w!help | an epic bot"), slash_commands=True)
+
+@bot.command(aliases=["latency"])
+async def ping(ctx):
+    await ctx.reply(f"🏓Pong! `{round(bot.latency*1000)}ms`")
+
+bot.run(config.token)
