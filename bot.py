59f4c1955d0ea2689a018e7d6c4424f196b8dc8b
whyfai <74695568+whyfai@users.noreply.github.com>
2021-12-17 16:54:35 +0700

    Make code closer to PEP 8 standards

--- bot.py
+++ bot.py
@@ -4,19 +4,31 @@ from discord.ext import commands
 from discord.ext.commands.bot import when_mentioned_or
 import asyncpg
 
-bot = commands.Bot(command_prefix=when_mentioned_or("w!", "w?"), activity=discord.Activity(type=discord.ActivityType.playing, name="w!help | an epic bot"), slash_commands=True)
+bot = commands.Bot(
+    command_prefix=when_mentioned_or("w!", "w?"),
+    activity=discord.Activity(
+        type=discord.ActivityType.playing,
+        name="w!help | an epic bot"),
+    slash_commands=True)
+
 
 @bot.event
 async def on_ready():
     print(f"{bot.user} is now online!")
 
+
 @bot.command(aliases=["latency"])
 async def ping(ctx):
     await ctx.reply(f"🏓Pong! `{round(bot.latency*1000)}ms`")
 
+
 @bot.command()
 async def invite(ctx):
-    await ctx.reply("With slash commands: https://discord.com/api/oauth2/authorize?client_id=775601025272905739&permissions=8&scope=bot%20applications.commands \nWithout slash commands: https://discord.com/api/oauth2/authorize?client_id=775601025272905739&permissions=8&scope=bot\n\nIf you are trying to add slash commands to a server that already has the bot use this link: https://discord.com/api/oauth2/authorize?client_id=775601025272905739&scope=applications.commands")
+    slash_cmd_invite = "https://discord.com/api/oauth2/authorize?client_id=775601025272905739&permissions=8&scope=bot%20applications.commands"
+    non_slash_cmd_invite = "https://discord.com/api/oauth2/authorize?client_id=775601025272905739&permissions=8&scope=bot"
+    slash_cmd_link = "https://discord.com/api/oauth2/authorize?client_id=775601025272905739&scope=applications.commands"
+    await ctx.reply(f"With slash commands: {slash_cmd_invite} \nWithout slash commands: {non_slash_cmd_invite}\n\nIf you are trying to add slash commands to a server that already has the bot use this link: {slash_cmd_link}")
+
 
 @bot.command(aliases=["calc"])
 async def calculate(ctx, num1, operation, num2):
